8cf3c2d98fc7905118ddc4268e96e28eb2d24f1f
Maple <2496155694@qq.com>
2020-01-03 11:41:55 +0800

    application 配置文件修改

--- HtxkEmsm-admin/src/main/java/com/RuoYiServletInitializer.java
+++ HtxkEmsm-admin/src/main/java/com/RuoYiServletInitializer.java
@@ -1,4 +1,4 @@
-package com.ruoyi;
+package com;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
